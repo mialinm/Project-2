cdc20477fd4d665f80359358e3dd9dbaaeb867fd
Mia <mia.minaya@lc.cuny.edu>
2023-10-02 22:21:35 -0400

    9

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 /*----- constants -----*/
 
 const winningCombos = [
+
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -9,6 +10,7 @@ const winningCombos = [
     [2, 5, 8],
     [0, 4, 8],
     [2, 4, 6]
+
     ];
 
 /*----- app's state (variables) -----*/
@@ -33,37 +35,64 @@ document.getElementById('reset-button').addEventListener('click', init);
 /*----- functions -----*/
 
 function getWinner() {
+
     let winner = null;
+
     winningCombos.forEach(function(combo, index) {
+
         if (board[combo[0]] && board[combo[0]] === board[combo[1]] && board[combo[0]] === board[combo[2]]) winner = board[combo[0]];
         });
+
         return winner ? winner : board.includes('') ? null : 'T';
+
 };
 
 function handleTurn() {
+
     let idx = squares.findIndex(function(square) {
+
         return square === event.target;
+
     });
+
     board[idx] = turn;
     turn = turn === 'X' ? 'O' : 'X';
     win = getWinner();
     render();
+
 };
 
 function init() {
+
     board = [
     '', '', '',
     '', '', '',
     '', '', ''
     ];
     render();
+
 };
 
 function render() {
+
     board.forEach(function(mark, index) {
+
     squares[index].textContent = mark;
+
     });
+
     messages.textContent = win === 'T' ? `That's a tie, queen!` : win ? `${win} wins the game!` : `It's ${turn}'s turn!`;
+
+    if (win === 'X') {
+
+        xScore++;
+
+    } else if (win === 'O') {
+
+        oScore++;
+        
+    }
+
     };
 
 init();
